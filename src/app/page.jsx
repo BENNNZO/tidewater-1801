520d718d44b752df38ed729355d46cf97498b170
Benjamin Phillips <benjamin.phillips.webdev@gmail.com>
2024-06-10 16:55:31 -0700

    init

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Nav from "@/components/Nav";
+
+export default function Home() {
+	return (
+		<main>
+			<Nav />
+			hello world!
+		</main>
+	);
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useState, useEffect } from "react"
+
+export default function Nav() {
+    return (
+        <nav>
+            <p>title</p>
+            <ul>
+                <li>
+                    <a href="">test</a>
+                </li>
+                <li>
+                    <a href="">test</a>
+                </li>
+                <li>
+                    <a href="">test</a>
+                </li>
+                <li>
+                    <a href="">test</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
